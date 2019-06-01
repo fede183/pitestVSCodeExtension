2e8415ebd939e71e1c6973b4df0140f34f4e4d24
fede183 <fede.183@hotmail.com>
2019-06-01 13:09:33 -0300

    Delete unnecesary requirement

--- test/stackBuild.test.js
+++ test/stackBuild.test.js
@@ -2,10 +2,6 @@
 
 // The module 'assert' provides assertion methods from node
 const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-const vscode = require('vscode');
 // const myExtension = require('../extension');
 
 //FileSystem
